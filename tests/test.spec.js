f46548a403e8d3eb718b4d77e81d130b2ef6fdf4
vepr1991 <vepr1991@gmail.com>
2024-10-18 04:32:32 +0500

    gitignore

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 const { MainPage } = require('./pages/MainPage');
 
-test('test', async ({ page }) => {
+test('CheckMainPage', async ({ page }) => {
     const mainPage = new MainPage(page);
     await page.goto('');
     await mainPage.CheckMainPage()
